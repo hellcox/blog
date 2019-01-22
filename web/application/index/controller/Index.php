cad939b890e1fc1378cc2594c88c53d00e051f62
hellcox <hellcox@163.com>
2019-01-22 21:28:37 +0800

    标题、关键词、描述

--- web/application/index/controller/Index.php
+++ web/application/index/controller/Index.php
@@ -12,12 +12,18 @@ class Index extends Controller
 
     public function index()
     {
+        echo config('idx_title');
+        $head['title'] = config('home_title');
+        $head['description'] = config('home_desc');
+        $head['keyword'] = config('home_keyword');
+
         $article = new Article();
         $list = $article->listAll(null);
         $page = $list->render();
 
         $this->assign('list', $list);
         $this->assign('page', $page);
+        $this->assign('head', $head);
         return $this->fetch('home');
     }
 
@@ -40,12 +46,21 @@ class Index extends Controller
         $article['views'] = $article['views'] + 1;
         $article->save();
 
+        $head['title'] = $article['title'].'-小豆豆博客';
+        $head['description'] = $article['description'];
+        $head['keyword'] = $article['keyword'];
+        $this->assign('head', $head);
+
         $this->assign('article', $article);
         return $this->fetch();
     }
 
     public function tools()
     {
+        $head['title'] = config('tool_title');
+        $head['description'] = config('tool_desc');
+        $head['keyword'] = config('tool_keyword');
+        $this->assign('head', $head);
         return $this->fetch();
     }
 }
