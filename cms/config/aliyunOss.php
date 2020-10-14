c9379d35837078378a8d13e8a19981e2dedf2647
hellcox <hellcox@163.com>
2020-10-14 17:00:58 +0800

    Update aliyunOss.php

--- cms/config/aliyunOss.php
+++ cms/config/aliyunOss.php
@@ -2,8 +2,8 @@
 
 return [
     // 应用名称
-    'keyId' => 'LTAIDqhXLvMp7x2E',
-    'keySecret' => 'WLJzacpU9AZx5ggiipF8s1JhDK2TQ1',
+    'keyId' => '',
+    'keySecret' => '',
     'endpoint' => 'oss-cn-shenzhen.aliyuncs.com',
     'bucket' => '930229',
     'httpUrl' => 'http://static.iuxiao.com',
